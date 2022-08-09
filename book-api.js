22f6427ddd0d27650f6ebe170143542569dc7e64
giovanafurlan <giovananelofurlan@gmail.com>
2022-08-09 10:27:17 -0300

    Alterando redirecionamento

--- book-api.js
+++ book-api.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 const port = 3000;
@@ -23,15 +24,15 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 
-app.post('/login', (req, res) => {
+axios.post('https://1c7a0275-b8b8-4235-b528-b1b625d0dbaa.zappsusercontent.com/appfiles/v2/1c7a0275-b8b8-4235-b528-b1b625d0dbaa/17.0/76109bc9ab21e62dc8c6a322a48e22771e741c436f3e75787fe7cac9dec51ba3/app/widget.html',{withCredentials: true}).then((req, res) => {
     const book = req.body;
     console.log(book);
     books.push(book);
 
-    res.redirect('https://desk.zoho.com/installExtension?extensionId=1c7a0275-b8b8-4235-b528-b1b625d0dbaa&version=11.0&hash=b2e8c876c99c38a07bf9c623a245d4a75c5710ea78610128d9c919d22d30f9a0');
+    // res.redirect('https://desk.zoho.com/installExtension?extensionId=1c7a0275-b8b8-4235-b528-b1b625d0dbaa&version=11.0&hash=b2e8c876c99c38a07bf9c623a245d4a75c5710ea78610128d9c919d22d30f9a0');
 });
 
-app.get('/data', (req, res) => {
+axios.get('https://1c7a0275-b8b8-4235-b528-b1b625d0dbaa.zappsusercontent.com/appfiles/v2/1c7a0275-b8b8-4235-b528-b1b625d0dbaa/17.0/76109bc9ab21e62dc8c6a322a48e22771e741c436f3e75787fe7cac9dec51ba3/app/widget.html/data').then((req, res) => {
     res.json(books);
 });
 
